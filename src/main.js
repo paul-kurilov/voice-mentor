2138694d047394e938e593407242125b817dd5d9
Paul Kurilov <support@kurilov.biz>
2023-04-26 02:50:51 +0300

    oggtomp3

--- src/main.js
+++ src/main.js
@@ -1,1 +1,41 @@
-console.log("yeees....");
+import config from "config";
+import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+import { ogg } from "./ogg.js";
+
+const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
+
+
+bot.on(message('voice'), async ctx => {
+  try {
+    const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
+    const userId = String(ctx.message.from.id)
+    const oggPath = await ogg.create(link.href, userId)
+    const mp3Path = await ogg.toMp3(oggPath, userId)
+
+    console.log(link.href)
+    await ctx.reply(JSON.stringify(link, null, 2))
+
+  } catch (e) {
+    console.log('Error while voice message', e.message);
+  }
+
+  
+})
+
+
+// bot.on(message('text'), async ctx => {
+//   await ctx.reply(JSON.stringify(ctx.message, null, 2))
+// })
+
+
+// bot.command('start', async (ctx) => {
+//   await ctx.reply(JSON.stringify(ctx.message, null, 2))
+// })
+
+
+bot.launch();
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
---
+++ src/ogg.js
@@ -0,0 +1,52 @@
+import axios from "axios"
+import ffmpeg from "fluent-ffmpeg"
+import installer from "@ffmpeg-installer/ffmpeg"
+import { createWriteStream } from "fs"
+import { dirname, resolve } from "path"
+import { fileURLToPath } from "url"
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+class OggConverter {
+  constructor() {
+    ffmpeg.setFfmpegPath(installer.path)
+  }
+
+  toMp3(input, output) {
+    try {
+      const outputPath = resolve(dirname(input), `${output}.mp3`)
+      return new Promise((resolve, reject) => {
+        ffmpeg(input)
+          .inputOption('-t 30')
+          .output(outputPath)
+          .on('end', () => resolve(outputPath))
+          .on('error', (e) => reject(e.message))
+          .run()
+      })
+    } catch (error) {
+      console.log('Error while creating mp3', error.message);
+    }
+  }
+
+  async create(url, filename) {
+    try {
+      const oggPath = resolve(__dirname, '../tmp-voices', `${filename}.ogg`)
+      const response = await axios({
+        method: 'get',
+        url,
+        responseType: 'stream'
+      })
+      return new Promise(resolve => {
+        const stream = createWriteStream(oggPath)
+        response.data.pipe(stream)
+        stream.on('finish', () => resolve(oggPath))
+      })
+      
+    } catch (error) {
+      console.log('Error while creating ogg', error.message)
+    }
+  }
+
+}
+
+export const ogg = new OggConverter()
